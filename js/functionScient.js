0b85d0b9cfbb4c7b83c0daee20e8be99c8c22c1e
DENIS-DONE-PROJECT <denis.avocat59@gmail.com>
2023-05-30 15:15:41 +0200

    ADD change from Nicolas

--- js/functionScient.js
+++ js/functionScient.js
@@ -1,67 +1,67 @@
-function fnAbs(num) {
-    return Math.abs(num);
-}
+// function fnAbs(num) {
+//     return Math.abs(num);
+// }
 
-function fnExp(num) {
-    return Math.exp(num);
-}
+// function fnExp(num) {
+//     return Math.exp(num);
+// }
 
-function fnFactorial(num) {
-    if (num === 0 || num === 1) return 1;
-    for (let i = num - 1; i >= 1; i--) {
-        num = num * i;
-    }
-    return num;
-}
+// function fnFactorial(num) {
+//     if (num === 0 || num === 1) return 1;
+//     for (let i = num - 1; i >= 1; i--) {
+//         num = num * i;
+//     }
+//     return num;
+// }
 
-function fnInverse(num) {
-    if (num !== 0) {
-        return 1 / num;
-    } else {
-        return "Divided by 0 not allowed";
-    }
-}
+// function fnInverse(num) {
+//     if (num !== 0) {
+//         return 1 / num;
+//     } else {
+//         return "Divided by 0 not allowed";
+//     }
+// }
 
-function fnLn(num) {
-    return fnLog(num) * Math.LN10;
-}
+// function fnLn(num) {
+//     return fnLog(num) * Math.LN10;
+// }
 
-function fnLog(num) {
-    if (num > 0) {
-        return Math.log10(num);
-    } else {
-        return "Number must be strictly positive";
-    }
-}
+// function fnLog(num) {
+//     if (num > 0) {
+//         return Math.log10(num);
+//     } else {
+//         return "Number must be strictly positive";
+//     }
+// }
 
-function fnMod(num, modulo) {
-    if (modulo !== 0) {
-        return num % modulo;
-    } else {
-        return "Modulo 0 not allowed";
-    }
-}
+// function fnMod(num, modulo) {
+//     if (modulo !== 0) {
+//         return num % modulo;
+//     } else {
+//         return "Modulo 0 not allowed";
+//     }
+// }
 
-function fnPourcent(num) {
-    return num / 100;
-}
+// function fnPourcent(num) {
+//     return num / 100;
+// }
 
-function fnSquare(num) {
-    return Math.pow(num, 2);
-}
+// function fnSquare(num) {
+//     return Math.pow(num, 2);
+// }
 
-function fnSquareRoot(num) {
-    if (num >= 0) {
-        return Math.sqrt(num);
-    } else {
-        return "Number must be positive or null ";
-    }
-}
+// function fnSquareRoot(num) {
+//     if (num >= 0) {
+//         return Math.sqrt(num);
+//     } else {
+//         return "Number must be positive or null ";
+//     }
+// }
 
-function fnXPowerY(x, power) {
-    return Math.pow(x, power);
-}
+// function fnXPowerY(x, power) {
+//     return Math.pow(x, power);
+// }
 
-function fn10PowerX(power) {
-    return Math.pow(10, power);
-}
+// function fn10PowerX(power) {
+//     return Math.pow(10, power);
+// }
